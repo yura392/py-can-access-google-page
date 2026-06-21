b241e85fd967c8ab7ca190766827770f68d7555a
yura <yura392@gmail.com>
2026-06-21 11:22:53 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,39 +1,33 @@
+from _pytest.monkeypatch import MonkeyPatch
 from app import main
 
-def test_accessible_when_url_valid_and_internet(monkeypatch):
-    # Мокаємо valid_google_url -> True
+
+def test_accessible_when_url_valid_and_internet(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(main, "valid_google_url", lambda url: True)
-    # Мокаємо has_internet_connection -> True
     monkeypatch.setattr(main, "has_internet_connection", lambda: True)
 
     result = main.can_access_google_page("https://www.google.com")
     assert result == "Accessible"
 
 
-def test_not_accessible_when_url_invalid(monkeypatch):
-    # Мокаємо valid_google_url -> False
+def test_not_accessible_when_url_invalid(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(main, "valid_google_url", lambda url: False)
-    # Мокаємо has_internet_connection -> True
     monkeypatch.setattr(main, "has_internet_connection", lambda: True)
 
     result = main.can_access_google_page("https://fake.com")
     assert result == "Not accessible"
 
 
-def test_not_accessible_when_no_internet(monkeypatch):
-    # Мокаємо valid_google_url -> True
+def test_not_accessible_when_no_internet(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(main, "valid_google_url", lambda url: True)
-    # Мокаємо has_internet_connection -> False
     monkeypatch.setattr(main, "has_internet_connection", lambda: False)
 
     result = main.can_access_google_page("https://www.google.com")
     assert result == "Not accessible"
 
 
-def test_not_accessible_when_both_invalid(monkeypatch):
-    # Мокаємо valid_google_url -> False
+def test_not_accessible_when_both_invalid(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(main, "valid_google_url", lambda url: False)
-    # Мокаємо has_internet_connection -> False
     monkeypatch.setattr(main, "has_internet_connection", lambda: False)
 
     result = main.can_access_google_page("https://fake.com")
